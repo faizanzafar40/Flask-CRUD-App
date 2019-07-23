d143047f765215a8bca8c9cf15239c4e4b8c0e7c
faizanzafar40 <faizanzafar40@gmail.com>
2019-07-23 20:04:34 +0200

    Initial commit

---
+++ bookmanager.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import redirect
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+
+@app.route('/', methods=["GET", "POST"])
+
+def home():
+    books = None
+    if request.form:
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+
+@app.route("/update", methods=["POST"])
+
+def update():
+    try:
+        new_title = request.form.get("new_title")
+        old_title = request.form.get("old_title")
+        book = Book.query.filter_by(title=old_title).first()
+        book.title = new_title
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
